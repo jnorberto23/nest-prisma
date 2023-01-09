a865bce09607372c92ac53678b8c0fa3dd26925d
jnorberto23 <joaovictor.norberto@outlook.com>
2023-01-09 17:24:33 -0300

    ✨ add prisma and validation integration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { MailService } from './mail/mail.service';
-
-@Controller()
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.mailService.sendEmail();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
